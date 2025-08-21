0c171048499b49982b2ada54c4b006f6bb2e1d1e
Nrrndr <narrendrasetyawanbahar@mail.ugm.ac.id>
2025-08-21 13:13:05 +0700

    upgrade button text set

--- Scripts/MainMenu/UpgradeButton.gd
+++ Scripts/MainMenu/UpgradeButton.gd
@@ -1,8 +1,18 @@
 class_name UpgradeButton extends Node
-var stat_type=0
+@export var stat_type=0
 var upgrade_cost=10
 var downgrade_cost=10
 @export var logo:TextureRect
 @export var Text:Texture2D
+@export var Value:Label
+@export var UpCost:Label
+@export var DownCost:Label
 func _ready() -> void:
 	logo.texture=Text
+	to_reset()
+func to_reset():
+	Value.text=str(MenuHub.stat[stat_type])
+	UpCost.text=str(upgrade_cost)
+	DownCost.text=str(downgrade_cost)
+	
+	
