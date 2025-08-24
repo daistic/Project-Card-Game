9d22dbb923c59699e68bb2f575964e795fb37b60
daistic <matotsuper@gmail.com>
2025-08-24 21:25:07 +0700

    fix: desperation bug

--- Scripts/Card Interfaces/WinnerDeck/AttackBasedOnHP.gd
+++ Scripts/Card Interfaces/WinnerDeck/AttackBasedOnHP.gd
@@ -2,7 +2,7 @@ class_name AttackBasedOnHP
 
 extends CardInterface
 
-@export var buffer: float = 10.0
+@export var damage: float = 6.9
 
 func get_card_damage(_damageable: Damageable) -> float:
-	return (_damageable.max_hp - buffer) / _damageable.cur_hp
+	return (((_damageable.max_hp - _damageable.cur_hp) / _damageable.max_hp) + 1.0) *damage
