0f8d1f8daeaaeb9bd3c66b166d94470aeb7cc055
daistic <matotsuper@gmail.com>
2025-09-16 19:42:37 +0700

    fix: battle manager reset problem

--- Stages/Main Menu/Screens/Play Screen/PlayScreen.gd
+++ Stages/Main Menu/Screens/Play Screen/PlayScreen.gd
@@ -5,8 +5,8 @@ extends TextureRect
 signal mode_selected
 
 func _on_mode_selected(is_hard_mode: bool) -> void:
-	BattleManager.set_story(is_hard_mode)
 	BattleManager.reset_battle_manager()
+	BattleManager.set_story(is_hard_mode)
 	BattleManager.set_starting_deck()
 	Global.load_data()
 
